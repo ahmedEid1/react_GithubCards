1601724281a95585d66c26a2bcb0cae5db17c25e
ahmedeid <kingmedo954@gmail.com>
2020-10-01 11:17:21 +0200

    creating the Card component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends React.Component {
+  render() {
+    return (
+        <div className="github-profile">
+            <img src="https://placehold.it/75" />
+            <div className="info">
+                <div className="name">name</div>
+                <div className="company">company</div>
+            </div>
+        </div>
+    )
+  }
 }
+
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <div className="header">{this.props.title}</div>
+          <Card />
+        </div>
+    )
+
+  }
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="Github Cards" />
   </React.StrictMode>,
   document.getElementById('root')
 );
